9f411368d1f389551c1ef9814a673b361badbef3
Axel Bernhard Dybdal Olin <axelolin5@gmail.com>
2026-05-13 11:51:05 +0200

    lies

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    /*float attackRange = Vector3.Distance();
+
+    // Update is called once per frame
+    void Update()
+    {
+        float attackScore = attackRange * dotAngleToPlayer;
+
+        float chaseScore = -attackRange * dotAngleToPlayer;
+
+        float patrolScore = -dotAngleToPlayer;
+
+        float bestMove = Mathf.Max(attackScore, chaseScore, patrolScore);
+
+
+        if(bestMove == attackScore)
+        {
+            AttackPlayer();
+        }
+        else if(bestMove == chaseScore)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            Patrol();
+        }
+    }
+    */
+}
